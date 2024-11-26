e35ef8157018fd59f99ca2c1b8caba25146ea804
NicoleJhone <105268456+NicoleJhone@users.noreply.github.com>
2024-11-26 15:51:04 +0400

    fixed

--- products.js
+++ products.js
@@ -1,47 +1,47 @@
 let products = [
     {
-        "id":1001,
-        "title":"Cat Food, 25lb bag",
-        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
-        "price":20.00,
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistable</em, organic goodness for you.",
+        "price": 20.00,
         "image": "images/product-fullsize.png",
-        "availableInventory":6,
-        "rating":2
+        "availableInventory": 11,
+        "rating": 2 
     },
     {
-        "id":1002,
-        "title":"Cat ball, 25lb bag",
-        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
-        "price":20.00,
-        "image": "images/product-fullsize.png",
-        "availableInventory":11,
-        "rating":5
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with for a very <strong>long</strong> time.",
+        "price": 2.99,
+        "image": "images/yarn.png",
+        "availableInventory": 7,
+        "rating": 3 
     },
     {
-        "id":1003,
-        "title":"Cat House, 25lb bag",
-        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
-        "price":20.00,
-        "image": "images/product-fullsize.png",
-        "availableInventory":20,
-        "rating":4
+        "id": 1003,
+        "title": "Bobby Fright Balls Cat Toy 4pcs",
+        "description": "Add a dash of Halloween enchantment to your cat's playtime with our Bobby Fright Balls Cat Toy Set – a captivating collection of 4 balls designed to keep your furry friend entertained, whether it's Halloween night or any day of the year!",
+        "price": 13.00,
+        "image": "images/bobbyfright.jpg",
+        "availableInventory": 13,
+        "rating": 5 
     },
     {
-        "id":1004,
-        "title":"Cat bell, 25lb bag",
-        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
-        "price":20.00,
-        "image": "images/product-fullsize.png",
-        "availableInventory":10,
-        "rating":1
+        "id": 1004,
+        "title": "Flamingo Flounder Moving Fish Electronic Cat Toy 29cm",
+        "description": "Searching for a stimulating cat toy? Check out Flamingo Moving Electronic Flounder Cat Toy 29cm. Hours of enjoyment can be had with this surprising, moving cat toy with catnip. With the provided USB cord, charging is simple. Offered in two colours: Grey, Orange.",
+        "price": 73.00,
+        "image": "images/fish.jpg",
+        "availableInventory": 15,
+        "rating": 4 
     },
     {
-        "id":1005,
-        "title":"Cat Food, 25lb bag",
-        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
-        "price":20.00,
-        "image": "images/product-fullsize.png",
-        "availableInventory":5,
-        "rating":3
+        "id": 1005,
+        "title": "All For Paws Dino Egg Spinner Cat Toy",
+        "description": "This Dino Egg Spinner toy spins 360 Degree and move around to keep your cat entertained and active. Works with 2AA batteries.",
+        "price": 20.00,
+        "image": "images/paw.jpeg",
+        "availableInventory": 5,
+        "rating": 2 
     }
-]   
+]
