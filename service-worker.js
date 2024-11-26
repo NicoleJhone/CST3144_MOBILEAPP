f981b58fdfcebda42f0ff7a6bd0cb515ad091e21
NicoleJhone <105268456+NicoleJhone@users.noreply.github.com>
2024-11-26 16:37:40 +0400

    zz

--- service-worker.js
+++ service-worker.js
@@ -20,26 +20,21 @@ self.addEventListener("install", (e) => {
   );
 });
 
-self.addEventListener('fetch', (e) => { 
-  console.log('[Service Worker] Fetched resource '+e.request.url); 
-  }); 
-  
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (r) {
-      // download the file if it is not in the cache
-      return (
-        r ||
-        fetch.apply(e.request).then(function (response) {
-          // add the new file to cache
-          return caches.open(cacheName).then(function (cache) {
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        })
-      );
-    })
-  );
+self.addEventListener("fetch", (e) => {
+  console.log("[Service Worker] Fetched resource " + e.request.url);
 });
 
-
+self.addEventListener('fetch', function (e) { 
+  e.respondWith( 
+  caches.match(e.request).then(function (r) { 
+  // Download the file if it is not in the cache,  
+  return r || fetch(e.request).then(function (response) { 
+  // add the new file to cache 
+  return caches.open(cacheName).then(function (cache) { 
+  cache.put(e.request, response.clone()); 
+  return response; 
+  }); 
+  }); 
+  }) 
+  ); 
+  }); 
