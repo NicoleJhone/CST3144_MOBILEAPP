860210736bc4c5ab239c5a3ac3b715b3d1b176e5
NicoleJhone <105268456+NicoleJhone@users.noreply.github.com>
2024-11-26 15:23:03 +0400

    change

---
+++ products.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+        "id":1001,
+        "title":"Cat Food, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/product-fullsize.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1002,
+        "title":"Cat ball, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/product-fullsize.png",
+        "availableInventory":11,
+        "rating":5
+    },
+    {
+        "id":1003,
+        "title":"Cat House, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/product-fullsize.png",
+        "availableInventory":20,
+        "rating":4
+    },
+    {
+        "id":1004,
+        "title":"Cat bell, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/product-fullsize.png",
+        "availableInventory":10,
+        "rating":1
+    },
+    {
+        "id":1005,
+        "title":"Cat Food, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/product-fullsize.png",
+        "availableInventory":5,
+        "rating":3
+    }
+]   
